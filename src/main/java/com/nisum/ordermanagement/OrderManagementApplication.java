21a65036dda16189c807f072157c1e066991e6c6
Shyam Vadikari <Shyam_Vadikari@gap.com>
2017-08-27 23:32:02 -0700

    security

--- src/main/java/com/nisum/ordermanagement/OrderManagementApplication.java
+++ src/main/java/com/nisum/ordermanagement/OrderManagementApplication.java
@@ -3,7 +3,7 @@ package com.nisum.ordermanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class)
 public class OrderManagementApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/nisum/ordermanagement/SecurityConfiguration.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.nisum.ordermanagement;
+
+/**
+ * @author SVadikari
+ *
+ */
+//@Configuration
+public class SecurityConfiguration {/*extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+}*/
+
+}
